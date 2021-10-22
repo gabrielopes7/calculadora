b34b534f4c6cbbbc5a6fc178d7969ea342a7cecd
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-22 13:52:11 -0300

    finalv1

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+let visor = document.getElementById("inputVisor");
+const somando = [];
+const subtraindo = [];
 
+function getNumber(n) {
+  visor.value += String(n);
 
-function number(this){
-    
+  let soma = document.getElementById("soma");
+  let subtrai = document.getElementById("subtrai");
+  let resultado = document.getElementById("resultado");
+
+  soma.setAttribute("onclick", `somar (${visor.value})`);
+  subtrai.setAttribute("onclick", `subtrair(${visor.value})`);
+  resultado.setAttribute("onclick", `resultado(${visor.value})`);
+}
+
+function somar(add) {
+  somando.push(add);
+  visor.value = "";
+  console.log(somando);
+}
+
+function subtrair(sub) {
+  subtraindo.push(sub);
+  subtraindo.sort(function (b, a) {
+    return a - b;
+  });
+  visor.value = "";
+  console.log(subtraindo);
+}
+
+function limpar(){
+    value.text = "";
+    somando = [];
+    subtraindo = [];
+}
+
+function resultado() {
+  if (visor.value != "") {
+    alert("Primeiro selecione a operação");
+  }
+
+  if (somando != "") {
+    let resultadoSoma = somando.reduce(
+      (resultadoSoma, somando) => resultadoSoma + somando
+    );
+    visor.value = resultadoSoma;
+  } else if (subtraindo != "") {
+    let resultadoSub = subtraindo.reduce(
+      (resultadoSub, subtraindo) => resultadoSub - subtraindo
+    );
+    visor.value = resultadoSub;
+  }
 }
