6b920f61679c6ed9d141a4375bb44f2282483715
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-29 20:01:55 -0300

    Update calculadora

--- script.js
+++ script.js
@@ -1,74 +1,44 @@
-let somando = [];
-let subtraindo = [];
-let multiplicando = [];
-let dividindo = [];
-
+let operador = "";
 
 let visor = document.getElementById("visor");
 let soma = document.getElementsByClassName("buttons")[11];
 let sub = document.getElementsByClassName("buttons")[7];
 let mult = document.getElementsByClassName("buttons")[3];
 
-// Pega o número para ser mostrado no visor.
+// Pega o número para ser mostrado no visor e adiciona o números na variavel operador.
 function getNumbers(n) {
   visor.value += String(n);
+  operador += String(n);
 }
 
-
-// Essa função está direcionando os números para seus respectivos arrays.
-function contas(op){
-  if ( op == soma){
-    somando.push(Number(visor.value));
-  } else if (op == sub){
-    subtraindo.push(Number(visor.value));
-  } else if (op == mult){
-    multiplicando.push(Number(visor.value));
-  } else{
-    dividindo.push(Number(visor.value));
+// Essa função está direcionando as operaçoes à variavel operador.
+function contas(op) {
+  if (op == soma) {
+    operador += `+`;
+  } else if (op == sub) {
+    operador += `-`;
+  } else if (op == mult) {
+    operador += `*`;
+  } else {
+    operador += `/`;
   }
-  visor.value = ""
+  visor.value = "";
 }
 
 // Essa função está testando os arrays, para saber qual resultado será mostrado a seguir.
 
 function resultado() {
-  if (visor.value != "") {
+  if (visor.value == "") {
     alert("Por favor, selecione uma operação!");
-  } else{
-    if (somando != "") {
-      let soma = somando.reduce((soma, somando) => soma + somando);
-      visor.value = soma;
-    }
-  
-    if (subtraindo != "") {
-      let subtrai = subtraindo.reduce(
-        (subtrai, subtraindo) => subtrai - subtraindo
-      );
-      visor.value = subtrai;
-    }
-
-    if (multiplicando != ""){
-      let multiplica = multiplicando.reduce(
-        (multiplica, multiplicando) => multiplica * multiplicando
-        );
-        visor.value = multiplica;
-    }
-
-    if (dividindo != ""){
-      let dividi = dividindo.reduce(
-        (dividi, dividindo) => dividi / dividindo
-      );
-      visor.value = dividi;
-    }
-  };
+  } else {
+    operador = eval(operador);
+    visor.value = operador;
+  }
 }
 
 // Essa função está limpando toda a calculadora.
 
 function limpar() {
   visor.value = "";
-  subtraindo = [];
-  somando = [];
-  multiplicando = [];
-  dividindo = [];
+  operador = "";
 }
